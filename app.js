4f2124eacc34061d05722e09a6ccf51f44f9816e
Orwa Shams <orwa73@outlook.com>
2021-06-01 15:26:42 +0300

    Changed the folders hierarchy to allow easer REST implementation
    Views folder contains what the client 'views'.
    layout contains the generic boilerplate to apply to all website pages, can be changed using DOM manipulation.
    To use the boilerplate or you want to add page to teacher or student. simply type:
    <% layout('./layouts/boilerplate') %>
       <h1>this is the student home page<h1>
    <% layout('./layouts/boilerplate') %>
    
    the h1 is copied as html <body> to the boilerplate.
    
    Included bootstrap in the project.
    
    Added partials folder in views which contains a generic navbar and footer, both included in boilerplate.ejs, this can be changed, we can add student navbar and teacher navbar for example and not a generic footer for all pages.

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const ejsMate = require("ejs-mate");
+
+const app = express();
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/teacher", (req, res) => {
+  res.render("teacher/index");
+});
+
+app.listen(3000, () => {
+  console.log("Listening to port 3000..");
+});
