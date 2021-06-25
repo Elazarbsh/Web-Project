4d87740a89a7092e39bc777be649295ea32edd90
Orwa Shams <orwa73@outlook.com>
2021-06-26 00:17:42 +0300

    Copied Elazar Auths

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const path = require("path");
 const ejsMate = require("ejs-mate");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const MongoStore = require("connect-mongo");
+const LocalStrategy = require("passport-local").Strategy;
+
 const User = require("./models/user");
 
 const app = express();
@@ -10,6 +15,24 @@ app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+    store: MongoStore.create({
+      mongoUrl:
+        "mongodb+srv://admin-elazar:elazarbsh@cluster0.4vrte.mongodb.net/userDB",
+      collectionName: "sessions",
+    }),
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect(
   "mongodb+srv://admin-elazar:elazarbsh@cluster0.4vrte.mongodb.net/userDB",
@@ -21,8 +44,48 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    console.log("auth called");
+    console.log(username + " " + password);
+    User.findOne({ username: username }, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        console.log("cant find user");
+        return done(null, false, { message: "Incorrect username." });
+      }
+      if (!(user.password == password)) {
+        console.log("incorrect password");
+        return done(null, false, { message: "Incorrect password." });
+      }
+      console.log("found user");
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/exams", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("exam/index");
+  } else {
+    res.redirect("/login");
+  }
 });
 
 app.get("/login", (req, res) => {
@@ -33,9 +96,14 @@ app.get("/register", (req, res) => {
   res.render("users/register");
 });
 
+app.get("/logout", (req, res, next) => {
+  req.logout();
+  res.redirect("/login");
+});
+
 app.post("/register", function (req, res) {
   const newUser = new User({
-    email: req.body.username,
+    username: req.body.username,
     password: req.body.password,
   });
 
@@ -44,28 +112,20 @@ app.post("/register", function (req, res) {
       console.log(err);
     } else {
       //what to do after registering a user.
+      req.session.isLogged = true;
       res.render("exams/index");
     }
   });
 });
 
-app.post("/login", function (req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  User.findOne({ email: username }, function (err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        if (foundUser.password === password) {
-          //what to do after user log in.
-          res.render("exams/index");
-        }
-      }
-    }
-  });
-});
+app.post(
+  "/login",
+  passport.authenticate("local", { failureRedirect: "/login" }),
+  function (req, res) {
+    req.session.isLogged = true;
+    res.redirect("/exams");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Listening to port 3000..");
